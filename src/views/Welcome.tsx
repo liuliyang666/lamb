ee771091cbcca6df0408ce70a2cd9f4d2400be2f
lilyyang <1592218619@qq.com>
2023-02-22 22:11:53 +0800

    feature: 第一个欢迎界面

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -11,7 +11,7 @@ export const Welcome = defineComponent({
           <img src={logo} />
           <h1>小羊记账</h1>
         </header>
-        <main>
+        <main class={s.main}>
           <RouterView />
         </main>
       </div>
