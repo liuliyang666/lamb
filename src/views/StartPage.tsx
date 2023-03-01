ac28d7631bd27edbcaa715b8bac0c2e09bd90da7
lilyyang <1592218619@qq.com>
2023-03-01 23:11:59 +0800

    创建 /items 和 、items/create 路由

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -6,6 +6,7 @@ import { Center } from "../shared/Center";
 import { Icon } from "../shared/Icon";
 import { Navbar } from "../shared/Navbar";
 import { Overlay } from "../shared/Overlay";
+import { RouterLink } from "vue-router";
 export const StartPage = defineComponent({
   setup: (props, context) => {
     const refOverlayVisible = ref(false);
@@ -26,9 +27,14 @@ export const StartPage = defineComponent({
           <Icon name="pot" class={s.pot} />
         </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button}>开始记账</Button>
+          <RouterLink to="/items/create">
+            <Button class={s.button}>开始记账</Button>
+          </RouterLink>
         </div>
-        <FloatButton iconName="add" />
+        <RouterLink to="/items/create">
+          <FloatButton iconName="add" />
+        </RouterLink>
+
         {refOverlayVisible.value && (
           <Overlay onClose={() => (refOverlayVisible.value = false)} />
         )}
