2aeb6372f8fbccf40a6240d6fe129eba3386c092
lilyyang <1592218619@qq.com>
2023-03-01 23:35:50 +0800

    创建 MainLayout 布局

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -7,6 +7,7 @@ import { Icon } from "../shared/Icon";
 import { Navbar } from "../shared/Navbar";
 import { Overlay } from "../shared/Overlay";
 import { RouterLink } from "vue-router";
+import { MainLayout } from "../layouts/MainLayout";
 export const StartPage = defineComponent({
   setup: (props, context) => {
     const refOverlayVisible = ref(false);
@@ -14,31 +15,33 @@ export const StartPage = defineComponent({
       refOverlayVisible.value = !refOverlayVisible.value;
     };
     return () => (
-      <div>
-        <Navbar>
-          {{
-            default: () => "小羊记账",
-            icon: () => (
-              <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
-            ),
-          }}
-        </Navbar>
-        <Center class={s.pot_wrapper}>
-          <Icon name="pot" class={s.pot} />
-        </Center>
-        <div class={s.button_wrapper}>
-          <RouterLink to="/items/create">
-            <Button class={s.button}>开始记账</Button>
-          </RouterLink>
-        </div>
-        <RouterLink to="/items/create">
-          <FloatButton iconName="add" />
-        </RouterLink>
+      <MainLayout>
+        {{
+          title: () => "小羊记账",
+          icon: () => (
+            <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
+          ),
+          default: () => (
+            <>
+              <Center class={s.pot_wrapper}>
+                <Icon name="pot" class={s.pot} />
+              </Center>
+              <div class={s.button_wrapper}>
+                <RouterLink to="/items/create">
+                  <Button class={s.button}>开始记账</Button>
+                </RouterLink>
+              </div>
+              <RouterLink to="/items/create">
+                <FloatButton iconName="add" />
+              </RouterLink>
 
-        {refOverlayVisible.value && (
-          <Overlay onClose={() => (refOverlayVisible.value = false)} />
-        )}
-      </div>
+              {refOverlayVisible.value && (
+                <Overlay onClose={() => (refOverlayVisible.value = false)} />
+              )}
+            </>
+          ),
+        }}
+      </MainLayout>
     );
   },
 });
