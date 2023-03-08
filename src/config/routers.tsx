0087fe2ea144ae53ea8ad8db44aa38a6139378ac
lilyyang <1592218619@qq.com>
2023-03-08 22:29:58 +0800

    添加 tag 相关页面

--- src/config/routers.tsx
+++ src/config/routers.tsx
@@ -12,6 +12,9 @@ import { StartPage } from "../views/StartPage";
 import { ItemPage } from "../views/ItemPage";
 import { ItemList } from "../components/item/ItemList";
 import { ItemCreate } from "../components/item/ItemCreate";
+import { TagCreate } from "../components/tag/TagCreate";
+import { TagEdit } from "../components/tag/TagEdit";
+import { TagPage } from "../views/TagPage";
 
 export const routes: RouteRecordRaw[] = [
   { path: "/", redirect: "/welcome" },
@@ -48,6 +51,14 @@ export const routes: RouteRecordRaw[] = [
     children: [
       { path: "", component: ItemList },
       { path: "create", component: ItemCreate },
+    ],
+  },
+  {
+    path: "/tag",
+    component: TagPage,
+    children: [
+      { path: "create", component: TagCreate },
+      { path: ":id", component: TagEdit },
     ],
   },
 ];
