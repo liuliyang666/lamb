cbb136d494ab3ff69a1e5902d758b8273736c263
lilyyang <1592218619@qq.com>
2023-03-10 21:39:38 +0800

    完成创建标签表单功能

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,10 +1,10 @@
-import { defineComponent, PropType, reactive } from "vue";
+import { defineComponent, PropType, reactive, toRaw } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
-import { Icon } from "../../shared/Icon";
-import s from "./TagCreate.module.scss";
-import { emojiList } from "../../shared/emojiList";
 import { Button } from "../../shared/Button";
 import { EmojiSelect } from "../../shared/EmojiSelect";
+import { Icon } from "../../shared/Icon";
+import { Rules, validate } from "../../shared/validate";
+import s from "./TagCreate.module.scss";
 export const TagCreate = defineComponent({
   props: {
     name: {
@@ -16,13 +16,32 @@ export const TagCreate = defineComponent({
       name: "",
       sign: "",
     });
+    const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({});
+    const onSubmit = (e: Event) => {
+      const rules: Rules<typeof formData> = [
+        { key: "name", type: "required", message: "必填" },
+        {
+          key: "name",
+          type: "pattern",
+          regex: /^.{1,4}$/,
+          message: "只能填 1 到 4 个字符",
+        },
+        { key: "sign", type: "required", message: "必填" },
+      ];
+      Object.assign(errors, {
+        name: undefined,
+        sign: undefined,
+      });
+      Object.assign(errors, validate(formData, rules));
+      e.preventDefault();
+    };
     return () => (
       <MainLayout>
         {{
           title: () => "新建标签",
           icon: () => <Icon name="left" onClick={() => {}} />,
           default: () => (
-            <form class={s.form}>
+            <form class={s.form} onSubmit={onSubmit}>
               <div class={s.formRow}>
                 <label class={s.formLabel}>
                   <span class={s.formItem_name}>标签名</span>
@@ -33,13 +52,13 @@ export const TagCreate = defineComponent({
                     ></input>
                   </div>
                   <div class={s.formItem_errorHint}>
-                    <span>必填</span>
+                    <span>{errors["name"] ? errors["name"][0] : "　"}</span>
                   </div>
                 </label>
               </div>
               <div class={s.formRow}>
                 <label class={s.formLabel}>
-                  <span class={s.formItem_name}>符号{formData.sign}</span>
+                  <span class={s.formItem_name}>符号 {formData.sign}</span>
                   <div class={s.formItem_value}>
                     <EmojiSelect
                       v-model={formData.sign}
@@ -47,11 +66,10 @@ export const TagCreate = defineComponent({
                     />
                   </div>
                   <div class={s.formItem_errorHint}>
-                    <span>必填</span>
+                    <span>{errors["sign"] ? errors["sign"][0] : "　"}</span>
                   </div>
                 </label>
               </div>
-
               <p class={s.tips}>记账时长按标签即可进行编辑</p>
               <div class={s.formRow}>
                 <div class={s.formItem_value}>
