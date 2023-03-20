2612efa76d52a664c480d9383a1660e1df4576e7
lilyyang <1592218619@qq.com>
2023-03-20 23:42:28 +0800

    登录页面会自定跳转到之前的页面

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -9,6 +9,7 @@ import axios from "axios";
 import { http } from "../shared/Http";
 import { useBool } from "../hooks/useBool";
 import { history } from "../shared/history";
+import { useRoute, useRouter } from "vue-router";
 export const SignInPage = defineComponent({
   setup: (props, context) => {
     const formData = reactive({
@@ -26,6 +27,8 @@ export const SignInPage = defineComponent({
       on: disabled,
       off: enable,
     } = useBool(false);
+    const router = useRouter();
+    const route = useRoute();
     const onSubmit = async (e: Event) => {
       e.preventDefault();
       Object.assign(errors, {
@@ -48,7 +51,9 @@ export const SignInPage = defineComponent({
       if (!hasError(errors)) {
         const response = await http.post<{ jwt: string }>("/session", formData);
         localStorage.setItem("jwt", response.data.jwt);
-        history.push("/");
+        // router.push('/sign_in?return_to='+ encodeURIComponent(route.fullPath))
+        const returnTo = route.query.return_to?.toString();
+        router.push(returnTo || "/");
       }
     };
     const onError = (error: any) => {
