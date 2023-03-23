90750ecd6e33968cbd214963b827f128d42e8eac
lilyyang <1592218619@qq.com>
2023-03-23 19:23:33 +0800

    删除测试代码

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -51,9 +51,7 @@ export const SignInPage = defineComponent({
       );
       if (!hasError(errors)) {
         const response = await http
-          .post<{ jwt: string }>("/session", formData, {
-            params: { _mock: "session" },
-          })
+          .post<{ jwt: string }>("/session", formData)
           .catch(onError);
         console.log(response);
         localStorage.setItem("jwt", response.data.jwt);
