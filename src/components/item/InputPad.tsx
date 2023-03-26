047d69c8814a7d4af0d5552118ea2b214258753e
lilyyang <1592218619@qq.com>
2023-03-26 21:51:53 +0800

    收集了四个字段kind、 tag_id、 happen_at 和 amount

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -5,13 +5,10 @@ import { Time } from "../../shared/time";
 import { DatetimePicker, Popup } from "vant";
 export const InputPad = defineComponent({
   props: {
-    name: {
-      type: String as PropType<string>,
-    },
+    happenAt: String,
+    amount: Number,
   },
   setup: (props, context) => {
-    const now = new Date();
-    const refDate = ref<Date>(now);
     const appendText = (n: number | string) => {
       const nString = n.toString();
       const dotIndex = refAmount.value.indexOf(".");
@@ -114,16 +111,20 @@ export const InputPad = defineComponent({
           refAmount.value = "0";
         },
       },
-      { text: "提交", onClick: () => {} },
+      {
+        text: "提交",
+        onClick: () =>
+          context.emit("update:amount", parseFloat(refAmount.value) * 100),
+      },
     ];
     const refDatePickerVisible = ref(false);
     const showDatePicker = () => (refDatePickerVisible.value = true);
     const hideDatePicker = () => (refDatePickerVisible.value = false);
     const setDate = (date: Date) => {
-      refDate.value = date;
+      context.emit("update:happenAt", date.toISOString());
       hideDatePicker();
     };
-    const refAmount = ref("0");
+    const refAmount = ref(props.amount ? (props.amount / 100).toString() : "0");
     return () => (
       <>
         <div class={s.dateAndAmount}>
@@ -131,14 +132,14 @@ export const InputPad = defineComponent({
             <Icon name="date" class={s.icon} />
             <span>
               <span onClick={showDatePicker}>
-                {new Time(refDate.value).format()}
+                {new Time(props.happenAt).format()}
               </span>
               <Popup
                 position="bottom"
                 v-model:show={refDatePickerVisible.value}
               >
                 <DatetimePicker
-                  value={refDate.value}
+                  value={props.happenAt}
                   type="date"
                   title="选择年月日"
                   onConfirm={setDate}
