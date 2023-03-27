ccb5c84bb49e32af58ea88b6a44c52a5245919c6
lilyyang <1592218619@qq.com>
2023-03-27 23:07:10 +0800

    可以新增记账了

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,6 +6,11 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+declare module "*.scss" {
+  const content: Record<string, any> = {};
+  export default content;
+}
+
 type JSONValue =
   | null
   | boolean
@@ -21,6 +26,14 @@ type Tag = {
   sign: string;
   kind: expenses | income;
 };
+type Item = {
+  id: number;
+  user_id: number;
+  amount: number;
+  tags_id: number[];
+  happen_at: string;
+  kind: expenses | income;
+};
 
 type Resources<T = any> = {
   resources: T[];
@@ -29,4 +42,12 @@ type Resources<T = any> = {
     per_page: number;
     count: number;
   };
+};
+
+type Resource<T> = {
+  resource: T;
+};
+
+type ResourceError = {
+  errors: Record<string, string[]>;
 };
