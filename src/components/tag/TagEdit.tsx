26a1dc2f00652d12bfd751d98015b675d724e032
lilyyang <1592218619@qq.com>
2023-03-29 16:35:39 +0800

    删除标签

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,11 +1,12 @@
 import { defineComponent, PropType, reactive } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
-import { Rules, validate } from "../../shared/validate";
 import { TagForm } from "./TagForm";
 import { Button } from "../../shared/Button";
 import s from "./Tag.module.scss";
 import { BackIcon } from "../../shared/BackIcon";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+import { Dialog } from "vant";
+import { http } from "../../shared/Http";
 export const TagEdit = defineComponent({
   setup: (props, context) => {
     const route = useRoute();
@@ -13,6 +14,25 @@ export const TagEdit = defineComponent({
     if (Number.isNaN(numberId)) {
       return () => <div>id 不存在</div>;
     }
+    const router = useRouter();
+    const onError = () => {
+      Dialog.alert({
+        title: "提示",
+        message: "删除失败",
+      });
+    };
+    const onDelete = async (options?: { withItems?: boolean }) => {
+      await Dialog.confirm({
+        title: "确认",
+        message: "你真的要删除吗？",
+      });
+      await http
+        .delete(`/tags/${numberId}`, {
+          withItems: options?.withItems ? "true" : "false",
+        })
+        .catch(onError);
+      router.back();
+    };
     return () => (
       <MainLayout>
         {{
@@ -22,13 +42,17 @@ export const TagEdit = defineComponent({
             <>
               <TagForm id={numberId} />,
               <div class={s.actions}>
-                <Button level="danger" class={s.removeTags} onClick={() => {}}>
+                <Button
+                  level="danger"
+                  class={s.removeTags}
+                  onClick={() => onDelete()}
+                >
                   删除标签
                 </Button>
                 <Button
                   level="danger"
                   class={s.removeTagsAndItems}
-                  onClick={() => {}}
+                  onClick={() => onDelete({ withItems: true })}
                 >
                   删除标签和记账
                 </Button>
