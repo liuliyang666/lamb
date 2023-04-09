5e9e34deb068045b8ff4b8f9e76981f880bd5def
lilyyang <1592218619@qq.com>
2023-04-09 17:09:40 +0800

    使用 store.$reset() 代替 reset

--- src/stores/useItemStore.tsx
+++ src/stores/useItemStore.tsx
@@ -7,7 +7,6 @@ type State = {
 };
 
 type Actions = {
-  reset: () => void;
   _fetch: (firstPage: boolean, startDate?: string, endDate?: string) => void;
   fetchItems: (startDate?: string, endDate?: string) => void;
   fetchNextPage: (startDate?: string, endDate?: string) => void;
@@ -21,11 +20,6 @@ export const useItemStore = (id: string | string[]) =>
       page: 0
     }),
     actions: {
-      reset() {
-        this.items = [];
-        this.hasMore = false;
-        this.page = 0;
-      },
       async _fetch(firstPage, startDate, endDate) {
         if (!startDate || !endDate) {
           return;
